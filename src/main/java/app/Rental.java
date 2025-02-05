6bdafa2eb8ea1c1f387c6ac8b9c6183aed7bd866
kris-toure <goliathheat@gmail.com>
2025-02-05 08:33:16 +0100

    Questions jusqu'au 9. pour les diagrammes de classe et séquence, faire avec bouml et autre

--- src/main/java/app/Rental.java
+++ src/main/java/app/Rental.java
@@ -1,18 +1,47 @@
 package app;
+
 class Rental {
-  private Movie movie;
-  private int daysRented;
-  
-  public Rental(Movie movie, int daysRented){
-    this.movie = movie;
-    this.daysRented = daysRented;
-  }
-  
-  public int getDaysRented() {
-    return daysRented;
-  }
-  
-  public Movie getMovie(){
-    return movie;
-  }
+    private Movie movie;
+    private int daysRented;
+
+    public Rental(Movie movie, int daysRented) {
+        this.movie = movie;
+        this.daysRented = daysRented;
+    }
+
+    public int getDaysRented() {
+        return daysRented;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public double getCharge() {
+        double result = 0;
+        switch (movie.getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (daysRented > 2)
+                    result += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result = daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (daysRented > 3)
+                    result += (daysRented - 3) * 1.5;
+                break;
+        }
+        return result;
+    }
+
+    public int getFrequentRenterPoints() {
+        int points = 1;
+        if ((movie.getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) {
+            points++;
+        }
+        return points;
+    }
 }
--- src/test/java/app/CustomerTest.java
+++ src/test/java/app/CustomerTest.java
@@ -18,69 +18,95 @@ public class CustomerTest {
     Rental rental = new Rental(movie, 3);
     customer.addRental(rental);
     String expectedStatement = "Rental Record for John Doe\n" +
-                               "\tThe Matrix\t3.5\n" +
-                               "Amount owed is 3.5\n" +
-                               "You earned 1 frequent renter points";
+        "\tThe Matrix\t3.5\n" +
+        "Amount owed is 3.5\n" +
+        "You earned 1 frequent renter points";
     assertEquals(expectedStatement, customer.statement());
   }
 
   @Test
   public void testStatementForRegularMovie() {
-    Customer customer = new Customer("John Doe");
-    Movie movie = new Movie("The Matrix", Movie.REGULAR);
-    Rental rental = new Rental(movie, 3);
-    customer.addRental(rental);
-    String expectedStatement = "Rental Record for John Doe\n" +
-                               "\tThe Matrix\t3.5\n" +
-                               "Amount owed is 3.5\n" +
-                               "You earned 1 frequent renter points";
-    assertEquals(expectedStatement, customer.statement());
+      Customer customer = new Customer("John Doe");
+      Movie movie = new Movie("The Matrix", Movie.REGULAR);
+      Rental rental = new Rental(movie, 3);
+      customer.addRental(rental);
+      String expectedStatement = "Rental Record for John Doe\n" +
+          "\tThe Matrix\t3.5\n" +
+          "Amount owed is 3.5\n" +
+          "You earned 1 frequent renter points";
+      assertEquals(expectedStatement, customer.statement());
   }
 
   @Test
   public void testStatementForNewReleaseMovie() {
-    Customer customer = new Customer("John Doe");
-    Movie movie = new Movie("Inception", Movie.NEW_RELEASE);
-    Rental rental = new Rental(movie, 2);
-    customer.addRental(rental);
-    String expectedStatement = "Rental Record for John Doe\n" +
-                               "\tInception\t6.0\n" +
-                               "Amount owed is 6.0\n" +
-                               "You earned 2 frequent renter points";
-    assertEquals(expectedStatement, customer.statement());
+      Customer customer = new Customer("John Doe");
+      Movie movie = new Movie("Inception", Movie.NEW_RELEASE);
+      Rental rental = new Rental(movie, 2);
+      customer.addRental(rental);
+      String expectedStatement = "Rental Record for John Doe\n" +
+          "\tInception\t6.0\n" +
+          "Amount owed is 6.0\n" +
+          "You earned 2 frequent renter points";
+      assertEquals(expectedStatement, customer.statement());
   }
 
   @Test
   public void testStatementForChildrensMovie() {
-    Customer customer = new Customer("John Doe");
-    Movie movie = new Movie("Toy Story", Movie.CHILDRENS);
-    Rental rental = new Rental(movie, 4);
-    customer.addRental(rental);
-    String expectedStatement = "Rental Record for John Doe\n" +
-                               "\tToy Story\t3.0\n" +
-                               "Amount owed is 3.0\n" +
-                               "You earned 1 frequent renter points";
-    assertEquals(expectedStatement, customer.statement());
+      Customer customer = new Customer("John Doe");
+      Movie movie = new Movie("Toy Story", Movie.CHILDRENS);
+      Rental rental = new Rental(movie, 4);
+      customer.addRental(rental);
+      String expectedStatement = "Rental Record for John Doe\n" +
+          "\tToy Story\t3.0\n" +
+          "Amount owed is 3.0\n" +
+          "You earned 1 frequent renter points";
+      assertEquals(expectedStatement, customer.statement());
   }
 
   @Test
   public void testStatementForMultipleRentals() {
-    Customer customer = new Customer("John Doe");
-    Movie movie1 = new Movie("The Matrix", Movie.REGULAR);
-    Movie movie2 = new Movie("Inception", Movie.NEW_RELEASE);
-    Movie movie3 = new Movie("Toy Story", Movie.CHILDRENS);
-    Rental rental1 = new Rental(movie1, 3);
-    Rental rental2 = new Rental(movie2, 2);
-    Rental rental3 = new Rental(movie3, 4);
-    customer.addRental(rental1);
-    customer.addRental(rental2);
-    customer.addRental(rental3);
-    String expectedStatement = "Rental Record for John Doe\n" +
-                               "\tThe Matrix\t3.5\n" +
-                               "\tInception\t6.0\n" +
-                               "\tToy Story\t3.0\n" +
-                               "Amount owed is 12.5\n" +
-                               "You earned 4 frequent renter points";
-    assertEquals(expectedStatement, customer.statement());
+      Customer customer = new Customer("John Doe");
+      Movie movie1 = new Movie("The Matrix", Movie.REGULAR);
+      Movie movie2 = new Movie("Inception", Movie.NEW_RELEASE);
+      Movie movie3 = new Movie("Toy Story", Movie.CHILDRENS);
+      Rental rental1 = new Rental(movie1, 3);
+      Rental rental2 = new Rental(movie2, 2);
+      Rental rental3 = new Rental(movie3, 4);
+      customer.addRental(rental1);
+      customer.addRental(rental2);
+      customer.addRental(rental3);
+      String expectedStatement = "Rental Record for John Doe\n" +
+          "\tThe Matrix\t3.5\n" +
+          "\tInception\t6.0\n" +
+          "\tToy Story\t3.0\n" +
+          "Amount owed is 12.5\n" +
+          "You earned 4 frequent renter points";
+      assertEquals(expectedStatement, customer.statement());
+  }
+
+  @Test
+  public void testStatementForNewReleaseMovieWithMoreThanOneDay() {
+      Customer customer = new Customer("John Doe");
+      Movie movie = new Movie("Inception", Movie.NEW_RELEASE);
+      Rental rental = new Rental(movie, 1);
+      customer.addRental(rental);
+      String expectedStatement = "Rental Record for John Doe\n" +
+          "\tInception\t3.0\n" +
+          "Amount owed is 3.0\n" +
+          "You earned 1 frequent renter points";
+      assertEquals(expectedStatement, customer.statement());
+  }
+
+  @Test
+  public void testStatementForChildrensMovieWithMoreThanThreeDays() {
+      Customer customer = new Customer("John Doe");
+      Movie movie = new Movie("Toy Story", Movie.CHILDRENS);
+      Rental rental = new Rental(movie, 3);
+      customer.addRental(rental);
+      String expectedStatement = "Rental Record for John Doe\n" +
+          "\tToy Story\t1.5\n" +
+          "Amount owed is 1.5\n" +
+          "You earned 1 frequent renter points";
+      assertEquals(expectedStatement, customer.statement());
   }
 }
